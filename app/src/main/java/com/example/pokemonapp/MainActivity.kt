5d5dacf05b93024ceec80898fc49e5c075fcd354
burak <buraksoftware1@gmail.com>
2023-03-24 21:28:22 +0300

    menu added into project and log out fun writed

--- app/src/main/java/com/example/pokemonapp/MainActivity.kt
+++ app/src/main/java/com/example/pokemonapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.pokemonapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.HiltAndroidApp
 
@@ -11,4 +12,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+
+        val exitMenuItem = menu.findItem(R.id.menu_exit)
+        exitMenuItem.setOnMenuItemClickListener {
+            finishAffinity()
+            true
+        }
+
+        return true
+    }
+
 }
