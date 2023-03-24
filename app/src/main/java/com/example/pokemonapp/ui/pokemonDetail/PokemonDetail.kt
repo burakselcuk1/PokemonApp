96f5b6b0a9f7cb3ad42cd7fc0568500f7b79b6c5
burak <design.burakk@gmail.com>
2023-03-24 15:55:51 +0300

    overlay manager added

--- app/src/main/java/com/example/pokemonapp/ui/pokemonDetail/PokemonDetail.kt
+++ app/src/main/java/com/example/pokemonapp/ui/pokemonDetail/PokemonDetail.kt
@@ -1,11 +1,16 @@
 package com.example.pokemonapp.ui.pokemonDetail
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.PixelFormat
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.*
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.bumptech.glide.Glide
 import com.example.pokemonapp.R
 import com.example.pokemonapp.base.BaseFragment
@@ -22,6 +27,7 @@ class PokemonDetail : BaseFragment<FragmentMainPageBinding, PokemonDetailViewMod
     layoutId = R.layout.fragment_main_page,
     viewModelClass = PokemonDetailViewModel::class.java
 ) {
+    @SuppressLint("MissingInflatedId")
     override fun onInitDataBinding() {
         val pokemonId = arguments?.getInt("pokemonId", -1) ?: -1
 
@@ -29,7 +35,38 @@ class PokemonDetail : BaseFragment<FragmentMainPageBinding, PokemonDetailViewMod
         viewModel.getPokemon(pokemonId)
 
         getDetailInformations()
+
+        binding.button.setOnClickListener {
+            activity?.finish()
+
+            val mParams: WindowManager.LayoutParams? = WindowManager.LayoutParams(
+                900,
+                900,
+                WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
+                WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN,
+                PixelFormat.TRANSLUCENT)
+
+            val testView = LayoutInflater.from(context).inflate(R.layout.overlay_hello, null)
+
+            val wm = context?.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+            wm.addView(testView, mParams)
+
+            val overlayName = testView.findViewById<TextView>(R.id.overlay_name)
+            val overlayImage = testView.findViewById<ImageView>(R.id.overlay_image)
+
+            observe(viewModel.pokemonDetail) {
+                overlayName.text = it.name.toString()
+                Glide.with(this).load(it.sprites.frontDefault).into(overlayImage)
+
+            }
+
+            testView.findViewById<Button>(R.id.button2).setOnClickListener {
+                activity?.finish()
+                wm.removeView(testView)
+            }
+        }
     }
+
 
     private fun getDetailInformations() {
         observeEvent(viewModel.statusData) {
